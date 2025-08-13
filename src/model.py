0a41d7cc188fe2eed4847d15138b103e256a5f67
Abel Marie <abelmarie49@gmail.com>
2025-08-13 19:09:38 +0300

    A single Transformer Encoder Block

--- src/model.py
+++ src/model.py
@@ -13,3 +13,25 @@ class PatchEmbedding(nn.Module):
         x = self.proj(x)
         x = x.flatten(2).transpose(1, 2)
         return x
+    
+class TransformerEncoder(nn.Module):
+    
+    def __init__(self, embed_dim, num_heads, mlp_dim, dropout):
+        super().__init__()
+        self.norm1 = nn.LayerNorm(embed_dim)
+        self.attn = nn.MultiheadAttention(embed_dim, num_heads, dropout=dropout, batch_first=True)
+        self.norm2 = nn.LayerNorm(embed_dim)
+        self.mlp = nn.Sequential(
+            nn.Linear(embed_dim, mlp_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(mlp_dim, embed_dim),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        attn_output, _ = self.attn(self.norm1(x), self.norm1(x), self.norm1(x))
+        x = x + attn_output
+        mlp_output = self.mlp(self.norm2(x))
+        x = x + mlp_output
+        return x
