bd583d2956e7296ca20518309932bd92da487294
Abel Marie <abelmarie49@gmail.com>
2025-08-14 20:08:21 +0300

    evaluation  function

--- src/training.py
+++ src/training.py
@@ -26,3 +26,24 @@ def train_one_epoch(model, dataloader, criterion, optimizer, device):
     accuracy = correct_preds / total_samples
     return avg_loss, accuracy
 
+def evaluate(model, dataloader, criterion, device):
+    """Evaluates the model on the validation set."""
+    model.eval()
+    total_loss = 0
+    correct_preds = 0
+    total_samples = 0
+
+    with torch.no_grad():
+        for images, labels in dataloader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+            total_loss += loss.item() * images.size(0)
+            _, preds = torch.max(outputs, 1)
+            correct_preds += (preds == labels).sum().item()
+            total_samples += labels.size(0)
+
+    avg_loss = total_loss / total_samples
+    accuracy = correct_preds / total_samples
+    return avg_loss, accuracy
